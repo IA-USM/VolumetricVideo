077050d914d6bc4284876a84ac55a095ddbc3b9b
pablodawson <pablodawson2@gmail.com>
2024-04-09 10:23:08 -0400

    use same points option

--- creator/run.py
+++ creator/run.py
@@ -2,13 +2,13 @@ import sys
 import os
 
 source = "D:/capturas/cocina/output/colmap_1"
-model = "output/new/sec"
+model = "output/cocinawarp"
 config = "configs/custom/main.json"
 total_duration = 299
 section_size = 10
 sections = total_duration // section_size
 
-for i in range(8, sections-1):
+for i in range(1, sections-1):
     cmd = f"python train_section.py -s {source} -m {model} --config {config} --section_idx {i}"
     print(cmd)
     os.system(cmd)
