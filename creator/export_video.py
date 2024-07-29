6ba2444ce528bcdd30b07225df6159d2b1e97332
pablo <titodawson@gmail.com>
2024-07-29 12:18:53 -0400

    working sog 12

--- creator/export_video.py
+++ creator/export_video.py
@@ -47,8 +47,6 @@ def create_frame_image(pc, timestamp, xyz_min=-1, xyz_max=1):
     
     pointopacity = pc._opacity
     
-    pc.trbfoutput = trbfoutput
-
     tforpoly = trbfdistanceoffset.detach()
     means3D = pc.get_xyz
     means3d_final = means3D +  pc._motion[:, 0:3] * tforpoly + pc._motion[:, 3:6] * tforpoly * tforpoly + pc._motion[:, 6:9] * tforpoly *tforpoly * tforpoly
@@ -64,22 +62,22 @@ def create_frame_image(pc, timestamp, xyz_min=-1, xyz_max=1):
 
     # colors
     colors = to_numpy_img(colors_final)
-    colors = (colors - min_thresholds["_colors"]) / (max_thresholds["_colors"] - min_thresholds["_colors"]) * 65535
+    colors = ((colors - min_thresholds["_colors"]) / (max_thresholds["_colors"] - min_thresholds["_colors"])).clip(0,1) * 65535
     colors = colors.astype(np.uint16)
 
     # rotations
     rotations = to_numpy_img(rotations_final)
-    rotations = (rotations - min_thresholds["_rotation"]) / (max_thresholds["_rotation"] - min_thresholds["_rotation"]) * 65535
+    rotations = ((rotations - min_thresholds["_rotation"]) / (max_thresholds["_rotation"] - min_thresholds["_rotation"])).clip(0,1) * 65535
     rotations = rotations.astype(np.uint16)
 
     # opacities
     opacities = to_numpy_img(opacities_final)
-    opacities = (opacities - min_thresholds["_opacity"]) / (max_thresholds["_opacity"] - min_thresholds["_opacity"]) * 65535
+    opacities = ((opacities - min_thresholds["_opacity"]) / (max_thresholds["_opacity"] - min_thresholds["_opacity"])).clip(0,1) * 65535
     opacities = opacities.astype(np.uint16)
 
     # scales
     scales = to_numpy_img(pc._scaling)
-    scales = (scales - min_thresholds["_scaling"]) / (max_thresholds["_scaling"] - min_thresholds["_scaling"]) * 65535
+    scales = ((scales - min_thresholds["_scaling"]) / (max_thresholds["_scaling"] - min_thresholds["_scaling"])).clip(0,1) * 65535
     scales = scales.astype(np.uint16)
 
     alpha_ones = np.ones((scales.shape[0], scales.shape[1], 1), dtype=np.uint16) * 65535
@@ -114,6 +112,8 @@ if __name__ == "__main__":
     xyz_min = 0
     xyz_max = 0
 
+    sorted_sections = sorted_sections[:-1]
+
     for idx, section in enumerate(sorted_sections):
                 
         time_range = [idx*args.section_size, (idx+1)*args.section_size]
@@ -144,7 +144,7 @@ if __name__ == "__main__":
                 with open(os.path.join(outpath, "min_max.txt"), "w") as f:
                     f.write(f"{xyz_min},{xyz_max}")
             
-            t = [frame/args.section_size for frame in range(time_range[0], time_range[1])]
+            t = [frame/args.section_size for frame in range(0, args.section_size)]
 
             for i, timestamp in tqdm(enumerate(t), total=len(t)):
                 frame_image = create_frame_image(gaussians, timestamp, xyz_min, xyz_max)
@@ -152,10 +152,11 @@ if __name__ == "__main__":
     
     print("Encoding video...")
     # encode results in a video
+    # -x265-params lossless=1 for lossless
     (
         ffmpeg
         .input(os.path.join(outpath, "frame_%05d.png"), framerate=args.fps)
-        .output(os.path.join(outpath ,"scene.mp4"), pix_fmt="yuva444p10le", 
-                vcodec="libx264", preset="slow", crf=15)
+        .output(os.path.join(outpath ,"scene.mkv"), pix_fmt="yuv420p10le",
+                vcodec="libx265", preset="medium", crf=5, tune="grain")
         .run()
     )
---
+++ creator/run_sog.py
@@ -0,0 +1,13 @@
+import os
+
+source = "D:/capturas/cocina/dataset/colmap_1"
+model = "output/cocinamil"
+config = "configs/custom/main.json"
+total_duration = 297
+section_size = 6
+sections = total_duration // section_size
+
+for i in range(0, sections-1):
+    cmd = f"python train_section_sog.py -s {source} -m {model} --config {config} --section_idx {i}"
+    print(cmd)
+    os.system(cmd)
